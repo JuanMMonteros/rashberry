a5e755684a70a600cf03e32d042bbc954c3083fa
PappasFlow <joaquindosmil@gmail.com>
2023-11-07 22:07:31 -0300

    comunicacion serial anda

--- TP4/ej8/recive.c
+++ TP4/ej8/recive.c
@@ -10,7 +10,7 @@ int serial_port;
 
 serial_port = serialOpen("/dev/serial0",9600);
 int caracter[20];
-for(int i=0;i<20;i++){
+for(int i=0;;i++){
 if(caracter[i]!='\0'){
 caracter[i]=serialGetchar(serial_port);
 printf("%c",caracter[i]);}
