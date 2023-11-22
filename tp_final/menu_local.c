9efb0d175f58e600905930a4be2dd9026d3656cd
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-11-22 13:00:16 -0300

    menu_local

---
+++ tp_final/menu_local.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "my_header.h"
+
+char read_keyboard();
+
+int menu_local() {
+    int w = 1;
+    int c;
+    while(w==1){
+    	printf("Ingrese numero de la secuencia de luces o exit (e) para salir:\n");
+    	c = read_keyboard();
+    	switch (c){ //case para juego de luces o salir
+					case 's':
+					case 'S':
+					case 'e':
+					case 'E': //salir menu 
+						printf("Saliendo del menu principal \n");
+						w=0;
+						break;
+					default: 
+						if ( control_leds( (int)c - 48 ) ) //llama a control leds si algo falla este debuelve 1 si todo esta ok 0 
+							printf("Parece que ocurrio un error al ejecutar esa secuencia, prueba nuevamente\n");
+						break;
+		}
+    }
+
+    return 0;
+}
+
+
+
+//funcion que lee tecla del teclado la imprime y salta de linea 
+char read_keyboard(){
+	char c;
+	c =  getchar();
+	printf("%c \n",c);
+	return c;
+}
