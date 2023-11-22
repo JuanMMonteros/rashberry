667766540f00e34093089b00af1074909dd4f380
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-11-22 16:04:41 -0300

    Add files via upload

---
+++ tp_final/fun_nointerrup.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/select.h>
+
+
+int conditionEND() {
+    fd_set rfds;
+    struct timeval tv;
+    int retval;
+
+        FD_ZERO(&rfds);
+        FD_SET(STDIN_FILENO, &rfds);
+
+        tv.tv_sec = 0;
+        tv.tv_usec = 0;
+
+        retval = select(STDIN_FILENO + 1, &rfds, NULL, NULL, &tv);
+        if (retval == -1) {
+            perror("select()");
+        } else if (retval) {
+            if (FD_ISSET(STDIN_FILENO, &rfds)) {
+                char c;
+                read(STDIN_FILENO, &c, 1);
+                printf("Tecla presionada: %c\n", c);
+                return 0;
+            }
+        } else {
+            // No se presionó ninguna tecla
+            return 1;
+        }
+    
+
+}
