7e2c85519d60e09d117eec4bdfb31e3aeeffb517
PappasFlow <joaquindosmil@gmail.com>
2023-11-21 23:15:54 -0300

    leds.c

---
+++ tp_final/leds.c
@@ -0,0 +1,10 @@
+#include <wiringPi.h>
+leds[8]={23,24,25,12,16,20,21,16}
+int main(){
+wiringPiSetup();
+for(int i=0;i<8;i++){
+pinMode(leds[i],OUTPUT);}
+for(int=0;i<8;i++){
+digitalWrite(leds[i],1);}
+return 0;
+}
