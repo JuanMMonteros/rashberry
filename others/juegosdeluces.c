ee52ed8bcbc4bdf3ca752ac9baecb8213bcdf6ee
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-11-23 09:51:38 -0300

    Update juegosdeluces.c

--- others/juegosdeluces.c
+++ others/juegosdeluces.c
@@ -7,15 +7,15 @@ void jump(int vel, volatile int vel_flecha, volatile int salida){
 		int k=7;
 		for(int i=0; i<8; i++){
 			if(i!=4){
-				digitalWrite[i,1];
+				digitalWrite(i,1);
 				delayMicroseconds((vel+(vel_flecha))*M);
-				digitalWrite[i,0];
+				digitalWrite(i,0);
 			}
 			if(salida==1)
 				return;
-			digitalWrite[k,1];
+			digitalWrite(k,1);
 			delayMicroseconds((vel+(vel_flecha))*M);
-			digitalWrite[k,0];
+			digitalWrite(k,0);
 			k--;
 			if(salida==1)
 				return;
@@ -27,16 +27,16 @@ void bounce(int vel, volatile int vel_flecha, volatile int salida){
 	while(salida!=1){
 		for (int i=0; i<8; i++){
 			for(int j=(8-i);j>0; j--){
-				digitalWrite[(j-1),1];
+				digitalWrite((j-1),1);
 				delayMicroseconds((vel+(vel_flecha))*M);
-				digitalWrite[(j-1),0];
+				digitalWrite((j-1),0);
 				if(salida==1)
 					return;
 			}
 			for(int k=0; k<(7-i);j++){
-				digitalWrite[k,1];
+				digitalWrite(k,1);
 				delayMicroseconds((vel+(vel_flecha))*M);
-				digitalWrite[k,0];
+				digitalWrite(k,0);
 				if(salida==1)
 					return;
 			}
