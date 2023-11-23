278b99c8f8f9bd9868b4c685adf42a9bcfa2fcc9
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-11-23 11:06:14 -0300

    Update juegosdeluces.c

--- tp_final/juegosdeluces.c
+++ tp_final/juegosdeluces.c
@@ -4,6 +4,8 @@
 
 #include "my_header.h"
 
+void act_time();
+
 int timeb=0; //variable global tiempo
 
 void jump(int vel){
@@ -12,10 +14,7 @@ void jump(int vel){
 		int k=7;
 		for(int i=0; i<8; i++){
 			if(i!=4){
-				//ajuste tiempo para los delays
-			timeb += up_dw();
-			if(timeb<0)       //limita a numeros positivos
-				timeb=0;
+				act_time();
 				
 				digitalWrite(led[i],1);
 				delay((vel+timeb)*M);
@@ -33,26 +32,37 @@ void jump(int vel){
 	}
 }
 	
-void bounce(int vel, volatile int vel_flecha, volatile int salida){
+void bounce(int vel){
 	int led[8]={4,5,6,26,27,28,29,25};
-	while(salida!=1){
+	while(1){
 		for (int i=0; i<8; i++){
 			for(int j=(8-i);j>0; j--){
+				act_time();
+				
 				digitalWrite((led[j]-1),1);
 				delay((vel+(vel_flecha))*M);
 				digitalWrite((led[j]-1),0);
-				if(salida==1)
+				if(condition_end() )
 					return;
 			}
 			for(int k=0; k<(7-i);k++){
+				act_time();
+				
 				digitalWrite(led[k],1);
 				delay((vel+(vel_flecha))*M);
 				digitalWrite(led[k],0);
-				if(salida==1)
+				if(condition_end() )
 					return;
 			}
 		}
 	}
+}
+
+void act_time(){
+	//ajuste tiempo para los delays
+	timeb += up_dw();
+	if(timeb<0)       //limita a numeros positivos
+		timeb=0;
 }
 	
 
