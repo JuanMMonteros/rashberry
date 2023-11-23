21fbcb9416b9ccc0584298efec78ec0998fc7429
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-11-23 10:05:57 -0300

    Update juegosdeluces.c

--- tp_final/juegosdeluces.c
+++ tp_final/juegosdeluces.c
@@ -2,20 +2,24 @@
 #include <wiringPi.h>
 #define M 1000
 
+#include "my_header.h"
+
+
 void jump(int vel, volatile int vel_flecha, volatile int salida){
+	int led[8]={4,5,6,26,27,28,29,25};
 	while(salida!=1){
 		int k=7;
 		for(int i=0; i<8; i++){
 			if(i!=4){
-				digitalWrite(i,1);
+				digitalWrite(led[i],1);
 				delayMicroseconds((vel+(vel_flecha))*M);
-				digitalWrite(i,0);
+				digitalWrite(led[i],0);
 			}
 			if(salida==1)
 				return;
-			digitalWrite(k,1);
+			digitalWrite(led[k],1);
 			delayMicroseconds((vel+(vel_flecha))*M);
-			digitalWrite(k,0);
+			digitalWrite(led[k],0);
 			k--;
 			if(salida==1)
 				return;
@@ -24,19 +28,20 @@ void jump(int vel, volatile int vel_flecha, volatile int salida){
 }
 	
 void bounce(int vel, volatile int vel_flecha, volatile int salida){
+	int led[8]={4,5,6,26,27,28,29,25};
 	while(salida!=1){
 		for (int i=0; i<8; i++){
 			for(int j=(8-i);j>0; j--){
-				digitalWrite((j-1),1);
+				digitalWrite((led[j]-1),1);
 				delayMicroseconds((vel+(vel_flecha))*M);
-				digitalWrite((j-1),0);
+				digitalWrite((led[j]-1),0);
 				if(salida==1)
 					return;
 			}
 			for(int k=0; k<(7-i);k++){
-				digitalWrite(k,1);
+				digitalWrite(led[k],1);
 				delayMicroseconds((vel+(vel_flecha))*M);
-				digitalWrite(k,0);
+				digitalWrite(led[k],0);
 				if(salida==1)
 					return;
 			}
