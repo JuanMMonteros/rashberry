1d4d2aebf43db9d8b7611dac49894441f1a916a5
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-11-23 10:05:42 -0300

    Update my_header.h

--- tp_final/my_header.h
+++ tp_final/my_header.h
@@ -4,9 +4,19 @@
 int control_leds(int);
 int password();
 int menu_local();
-char read_keyboard();
+
+//limpia consola
 void clean_consol();
+
+//lectura teclas sin espera activa
 int condition_end();
 int up_dw();
+
+//lee caracter y lo escribe en pantalla
+char read_keyboard();
+
+//juegos luces por algoritmo
+void jump(int vel, volatile int vel_flecha, volatile int salida)
+void bounce(int vel, volatile int vel_flecha, volatile int salida)
 
 #endif
