28dda12c302be717e5a4b323021f0d860109363b
unknown <monterosjuan329@gmail.com>
2023-11-25 17:31:36 -0300

    anda por favor

--- TP4/ej8/send.c
+++ TP4/ej8/send.c
@@ -6,9 +6,11 @@ wiringPiSetup();
 int serial_port;
 serial_port = serialOpen("/dev/serial0",9600);
 char * caracter ="Hola Mundo";
-
+char okey;
 for(int i=0;i<20;i++){
 serialPutchar(serial_port,caracter[i]);
+scanf("%c",&okey);
+serialPutchar(serial_port,okey);
 serialFlush(serial_port);
 delay(10);
 }
