eb3c35a369c5793f4bb373bbe3df171e1d4bd032
JuanMMonteros <monterosjuan329@gmail.com>
2023-11-27 15:11:00 -0300

    Prueba comunicacion

--- TP4/ej8/recive.c
+++ TP4/ej8/recive.c
@@ -4,12 +4,12 @@
  int main(){
 wiringPiSetup();
 int serial_port;
-
 serial_port = serialOpen("/dev/serial0",9600);
 serialFlush(serial_port);
 char caracter;
 caracter=serialGetchar(serial_port);
 printf("%c",caracter);
 serialFlush(serial_port);
+serialClose(fd);
  return 0;
 }
