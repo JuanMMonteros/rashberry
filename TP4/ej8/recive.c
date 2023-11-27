7650c00a3d47286f9e9a9eca8f43bf81295baeb0
JuanMMonteros <82298700+JuanMMonteros@users.noreply.github.com>
2023-11-27 16:34:32 -0300

    Update recive.c

--- TP4/ej8/recive.c
+++ TP4/ej8/recive.c
@@ -6,8 +6,6 @@ wiringPiSetup();
 int serial_port;
 serial_port = serialOpen("/dev/serial0",9600);
 int caracter;
-serialFlush(serial_port);
-delay(10);
   while(1){
     caracter=serialGetchar(serial_port);
     if(caracter != -1){
