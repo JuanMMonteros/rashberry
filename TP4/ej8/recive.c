a3cfe86b8c9e291e2099b5536df0378358ccc5ce
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-11-27 15:42:24 -0300

    Update recive.c

--- TP4/ej8/recive.c
+++ TP4/ej8/recive.c
@@ -8,8 +8,10 @@ serial_port = serialOpen("/dev/serial0",9600);
 int caracter;
 serialFlush(serial_port);
 delay(10);
-caracter=serialGetchar(serial_port);
-printf("%d/n",caracter);
+  wile(1){
+    caracter=serialGetchar(serial_port);
+    printf("%d\n",caracter);
+  }
 serialClose(serial_port);
  return 0;
 }
