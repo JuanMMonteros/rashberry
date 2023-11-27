e813da8856a25e0dd3ed8e43507fb4daac22fb9a
JuanMMonteros <82298700+JuanMMonteros@users.noreply.github.com>
2023-11-27 20:54:02 -0300

    Update main.c

--- tp_final/main.c
+++ tp_final/main.c
@@ -23,7 +23,7 @@ struct termios term_orig;
     tcsetattr(STDIN_FILENO, TCSANOW, &term); // Configura el modo sin eco
 	//programas aqui abajo
 	
-	if(!sync()){
+	if(sync()==0){
 		printf("La comunicacioncion Remota esta disponible");
 	}
 	while(!read_keyboard()){
