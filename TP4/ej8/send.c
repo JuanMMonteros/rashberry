3a35405609ac316afd24c504dcd67ddcba6dd463
JuanMMonteros <82298700+JuanMMonteros@users.noreply.github.com>
2023-11-27 16:32:21 -0300

    Update send.c

--- TP4/ej8/send.c
+++ TP4/ej8/send.c
@@ -4,9 +4,9 @@
  int main(){
 wiringPiSetup();
 int serial_port;
-serial_port = serialOpen("/dev/serial0",19200);
+serial_port = serialOpen("/dev/serial0",9600);
 serialFlush(serial_port);
-unsigned char caracter =getchar();
+unsigned char caracter =65;
 serialPutchar(serial_port,caracter);
  return 0;
 }
