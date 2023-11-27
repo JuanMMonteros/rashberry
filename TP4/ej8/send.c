7d54f3b5d7e82963d172194f7ff379e5e2d9e9ab
joaquin pappano <114824831+PappasFlow@users.noreply.github.com>
2023-11-27 15:30:09 -0300

    Update send.c

--- TP4/ej8/send.c
+++ TP4/ej8/send.c
@@ -6,7 +6,7 @@ wiringPiSetup();
 int serial_port;
 serial_port = serialOpen("/dev/serial0",9600);
 serialFlush(serial_port);
-deley(10);
+delay(10);
 char caracter ='A';
 for(int i=0;i<24;i++){
 serialPutchar(serial_port,caracter);
