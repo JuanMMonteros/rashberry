8986c546e4f0dc1b7e23af0992b0b6bc3aa75aac
JuanMMonteros <82298700+JuanMMonteros@users.noreply.github.com>
2023-11-27 18:35:24 -0300

    Update ejemplo.c

--- TP4/ej8/ejemplo.c
+++ TP4/ej8/ejemplo.c
@@ -1,41 +1,23 @@
-/* comunicacion UART en Raspberry Pi usando C (Libreria WiringPi)
-*/
-/* uso: imprime caracter recibido desde el puerto serie*/
-/* Compila: gcc -o uartw uartw.c -lwiringPi*/
-/* Ejecuta: sudo ./uartw */
-/* Salir: Ctrl+c */
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-#include <wiringPi.h>
 #include <wiringSerial.h>
+#include <stdio.h>
 
-int main ()
-{
-  int serial_port ;
-  char dat;
-  if ((serial_port = serialOpen ("/dev/serial0", 9600)) < 0)			/* abrir el puerto serie */
-  //if ((serial_port = serialOpen ("/dev/ttyAMA0", 9600)) < 0)			/* abrir el puerto serie */
-  {
-    fprintf (stderr, "No pudo abrirse el dispositivo serie: %s\n", strerror (errno)) ;
-    return 1 ;
-  }
+int main() {
+    int serial_port;
+    char serial_data;
 
-  if (wiringPiSetup () == -1)							/* inicializa la configuracion de wiringPi */
-  {
-    fprintf (stdout, "No pudo comenzar wiringPi: %s\n", strerror (errno)) ;
-    return 1 ;
-  }
+    if ((serial_port = serialOpen("/dev/ttyS0", 9600)) < 0) {
+        fprintf(stderr, "Error al abrir el puerto serial\n");
+        return 1;
+    }
 
-  printf (" -------Comunicacion serie----\n") ;
-  while(1){
-	  
-	if(serialDataAvail(serial_port)) 		/* retorna el numero de caracteres disponibles para leer o -1*/
-	{
-	  dat = serialGetchar(serial_port);		/* retorna el siguiente caracter disponible en el dispositivo serial */	
-	  printf ("%c", dat);
-	  //serialPutchar(serial_port, dat);		/* envia un unico byte por el puerto serie indicado*/
-	}
+    serialPuts(serial_port, "Hola desde Raspberry Pi 4\n");
 
-  }
+    while (1) {
+        if (serialDataAvail(serial_port)) {
+            serial_data = serialGetchar(serial_port);
+            printf("Carácter recibido: %c\n", serial_data);
+        }
+    }
+
+    return 0;
 }
